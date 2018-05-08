a20232124b869bae16fc25909ba5c17aefc47600
Taziva <stmarovatsanga@gmail.com>
2018-05-08 23:40:43 +0100

    Add circle config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	setupFiles: ["<rootDir>/jest.setup.js"],
 	testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+	collectCoverage: true,
 	moduleNameMapper: {
 		"\\.(scss)$": "<rootDir>/node_modules/jest-css-modules"
 	}
