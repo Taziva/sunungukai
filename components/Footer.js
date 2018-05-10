892c916727294573eef89a6e368e4d46764076bc
Taziva <stmarovatsanga@gmail.com>
2018-05-10 01:32:14 +0100

    Add footer

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => (
+	<footer className="footer">
+		<div className="footer__content u-center-text">
+			<p className="footer__copyright">&copy; Idealisme Sombre</p>
+		</div>
+	</footer>
+);
+
+export default Footer;
