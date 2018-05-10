ca9308ba52377bd153a5c95ff018b1711e45aca4
Taziva <stmarovatsanga@gmail.com>
2018-05-10 21:53:08 +0100

    Add port

--- pages/blog.js
+++ pages/blog.js
@@ -8,7 +8,9 @@ import "../sass/main.scss";
 
 class Blog extends Component {
 	static async getInitialProps() {
-		let response = await axios.get("/api/posts");
+		let response = await axios.get("/api/posts", {
+			proxy: { port: 3000 }
+		});
 		return { posts: response.data };
 	}
 
