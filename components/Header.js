1fefbb1fcf16f60d1fcdfb402f6a6cadcef8e71f
Taziva <stmarovatsanga@gmail.com>
2018-07-30 01:40:43 +0100

    Create mobile view

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,11 @@ import NavLink from "./navigation/NavLink";
 import { Component } from "react";
 
 export default class Header extends Component {
-	state = { activeClass: "" };
+	state = { activeClass: "", responsiveClass: "" };
 
 	componentDidMount() {
 		window.addEventListener("scroll", event => {
-			let activeClass;
+			let activeClass = this.state.activeClass;
 			if (window.pageYOffset > 890) {
 				activeClass = "fixed";
 			} else {
@@ -18,11 +18,43 @@ export default class Header extends Component {
 		});
 	}
 
+	toggleResponsiveNav() {
+		const navigation = this.navigation;
+		const responsiveClass = "responsive";
+		const activeClass = this.state.responsiveClass;
+		if (!navigation.className.includes(responsiveClass)) {
+			this.setState({
+				responsiveClass: activeClass + responsiveClass
+			});
+		} else {
+			this.setState({
+				responsiveClass: activeClass.replace(responsiveClass, "")
+			});
+		}
+	}
+
 	render() {
 		return (
-			<nav className={`nav ${this.state.activeClass}`}>
-				<div className="nav__logo">
-					<p>Sunungukai</p>
+			<nav
+				className={`nav ${this.state.activeClass} ${
+					this.state.responsiveClass
+				}`}
+				ref={navigation => {
+					this.navigation = navigation;
+				}}
+			>
+				<div className="nav__row">
+					<div className="nav__logo">
+						<p>Sunungukai</p>
+					</div>
+					<div className="nav__responsive-toggle">
+						<a className="icon" onClick={this.toggleResponsiveNav.bind(this)}>
+							<img
+								src="static/images/menu-button-of-three-horizontal-lines.svg"
+								width={16}
+							/>
+						</a>
+					</div>
 				</div>
 				<ul className="nav__list">
 					<li>
